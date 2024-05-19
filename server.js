e374acffccfcd7693e485b3a9fa7a03f08073656
Naveen Sachdeva <naveen.sachdeva@sap.com>
2024-05-19 16:04:17 +0000

    added dependencies

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
-// const http = require("http");
+const express = require('express');
+const bodyparser = require('body-parser');
 
-// const routes = require("./routes");
+const app = express();
 
-// const server = http.createServer(routes);
+app.use(bodyparser.urlencoded({extended : false}));
 
-// server.listen(2000);
+app.use('/users',(req, res, next) => {
+    res.send("<form action = '/userdata' method = 'POST'><input type = 'text' name = 'title'> <button type = 'submit'>Add User</form>");
+    // next();
+})
+
+app.post('/userdata', (req, res, next) => {
+    console.log(req.body);
+    console.log("hello");
+    res.redirect('/');
+
+})
+
+app.use('/', (req, res, next) => {
+    res.send("<h1>This is an HTML Page</h1>");
+})
+
+
+// app.use('/',(req, res, next) => {
+//     console.log("in the second midlware!!");
+//     res.send("<h1>This is an HTML Page</h1>");
+//     //res.end();
+//     // next();
+// })
+
+// app.use('/',(req, res, next) => {
+//     console.log("in the third midlware!!");
+// })
+
+app.listen(2000);
